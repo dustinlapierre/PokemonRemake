c943b6ed0ba01f9eb25e55406eccf3b3e2a42e03
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-02-22 19:28:17 +0100

    Initial commit of Unity Project

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private bool isMoving;
+    private Vector2 input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            //Can't move horizontally and vertically at the same time
+            if (input.x != 0) input.y = 0;
+
+            if(input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.y += input.y;
+                targetPos.x += input.x;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
