1445015f0361829c1fe21df9c5e2a952ccfc8448
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-02-25 21:14:47 +0100

    Added movement collisions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
+
     private bool isMoving;
     private Vector2 input;
 
@@ -38,7 +40,8 @@ public class PlayerController : MonoBehaviour
                 targetPos.x += input.x;
 
                 //approach target over time
-                StartCoroutine(Move(targetPos));
+                if(IsWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
             }
         }
 
@@ -57,5 +60,15 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    //check if there is no SolidObject at the target pos
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos, 0.05f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+        return true;
     }
 }
