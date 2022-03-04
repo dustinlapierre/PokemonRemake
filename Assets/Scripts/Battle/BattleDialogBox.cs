af2b915c303047a974aabe3644db07998accc934
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-03-04 21:38:17 +0100

    Added references to various UI elements to the battle controller, began adding user action selections and state machines

---
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] Color highlightColor;
+
+    [SerializeField] Text dialogText;
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach(var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f/lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for(int i = 0; i < actionTexts.Count; i++)
+        {
+            if (i == selectedAction)
+                actionTexts[i].color = highlightColor;
+            else
+                actionTexts[i].color = Color.black;
+        }
+    }
+}
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -2,24 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState
+{
+    Start,
+    PlayerAction,
+    PlayerMove,
+    EnemyMove,
+    Busy
+}
+
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
+
+    BattleState state;
+    int currentAction;
 
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    public IEnumerator SetupBattle()
     {
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
 
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Pokemon);
+
+        yield return StartCoroutine(dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared."));
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    private void Update()
+    {
+        if(state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(currentAction < 1)
+                ++currentAction;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+                --currentAction;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                //Fight
+            }
+            else if (currentAction == 1)
+            {
+                //Run
+            }
+        }
     }
 }
